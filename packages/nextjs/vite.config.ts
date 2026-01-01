12166b02e2206be3cc08e78d5254c31b876533fd
r-ishino <r.ishino0413@gmail.com>
2026-01-01 16:18:10 +0900

    use vite build

---
+++ packages/nextjs/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      exclude: ['**/*.test.tsx', '**/*.spec.tsx'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        'next',
+        'next/link',
+        '@sample/ui',
+        '@sample/util',
+      ],
+    },
+    sourcemap: true,
+  },
+});
---
+++ packages/util/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      exclude: ['**/*.test.ts', '**/*.spec.ts'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    sourcemap: true,
+  },
+});
